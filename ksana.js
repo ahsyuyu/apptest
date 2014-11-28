e5162486949751b4950de1c4252d1e5a3c7d9b5c
ahsyuyu <ahsyuyu@gmail.com>
2014-11-28 16:49:14 +0800

    initialize

---
+++ ksana.js
@@ -0,0 +1,10 @@
+jsonp_handler({
+"version": "1",
+"build": 1,
+"title":"apptest",
+"date":"Fri Nov 28 2014 16:47:35 GMT+0800 (CST)",
+"minruntime": "1.3",
+"baseurl":"http://rawgit.com/ahsyuyu/apptest/master/",
+"description":"",
+"files":["index.html","build.js","build.css","jquery.js","react-with-addons.js","ksana.js","apptest.kdb"]
+})
