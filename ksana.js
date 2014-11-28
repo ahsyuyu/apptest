c85f67e2ec42a2c6a28c617b813673e875a968bb
ahsyuyu <ahsyuyu@gmail.com>
2014-11-28 16:50:42 +0800

    add build

--- ksana.js
+++ ksana.js
@@ -1,10 +1,32 @@
 jsonp_handler({
-"version": "1",
-"build": 1,
-"title":"apptest",
-"date":"Fri Nov 28 2014 16:47:35 GMT+0800 (CST)",
-"minruntime": "1.3",
-"baseurl":"http://rawgit.com/ahsyuyu/apptest/master/",
-"description":"",
-"files":["index.html","build.js","build.css","jquery.js","react-with-addons.js","ksana.js","apptest.kdb"]
+ "version": "1",
+ "build": 2,
+ "title": "apptest",
+ "date": "2014-11-28T08:50:21.691Z",
+ "minruntime": "1.3",
+ "baseurl": "http://rawgit.com/ahsyuyu/apptest/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "build.js",
+  "build.css",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "apptest.kdb"
+ ],
+ "filesizes": [
+  422,
+  268382,
+  624538,
+  315,
+  1195
+ ],
+ "filedates": [
+  "2014-11-28T08:47:35.000Z",
+  "2014-11-28T08:47:35.000Z",
+  "2014-11-28T08:47:35.000Z",
+  "2014-11-28T08:47:35.000Z",
+  "2014-11-28T08:48:12.000Z"
+ ]
 })
